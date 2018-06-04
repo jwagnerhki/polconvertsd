d9e521839d8ff4f28bb973c833d2ec13f135ce89
jwagner313 <oper@frontend.service>
2018-06-04 16:38:15 +0200

    no need for numpy in setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,8 @@
 from distutils.core import setup, Extension
-import numpy as np
-
 
 # COMPILE THE GLOBAL CROSS-POLARIZATION FRINGE FITTING.
 # IT NEEDS FFTW AND GSL:
 DO_SOLVE = True
-
-
-
 
 sourcefiles = ['_PolConvert.cpp','CalTable.cpp','DataIO.cpp',
                'DataIOFITS.cpp','DataIOSWIN.cpp','Weighter.cpp']
@@ -26,30 +21,20 @@ c_ext3 = Extension("_getAntInfo", sources=sourcefiles3,
                   libraries=['cfitsio'],
                   extra_link_args=["-Xlinker", "-export-dynamic"])
 
-
-
 if DO_SOLVE:
-  c_ext2 = Extension("_PolGainSolve", sources=sourcefiles2,
+    c_ext2 = Extension("_PolGainSolve", sources=sourcefiles2,
                   libraries=['gsl','fftw3'],
                   extra_compile_args=["-Wno-deprecated","-O3"],
                   extra_link_args=["-Xlinker", "-export-dynamic"])
 
 cfitsio='/usr/include/cfitsio'
-setup(
-    ext_modules=[c_ext], include_dirs=[cfitsio],
-)
 
+setup(ext_modules=[c_ext], include_dirs=[cfitsio])
 
-setup(
-    ext_modules=[c_ext3], include_dirs=[cfitsio],
-)
-
-
+setup(ext_modules=[c_ext3], include_dirs=[cfitsio])
 
 if DO_SOLVE:
-  setup(
-    ext_modules=[c_ext2],
-  )
+    setup(ext_modules=[c_ext2])
 
 
 
